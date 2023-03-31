a533504e405599dc48c232a27e66acf96a045f9a
Adham_Emam <mrado372002@gmail.com>
2023-03-31 05:56:38 +0200

    upload

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ img, date, title, blogUrl }) => {
+  return (
+    <div className="gpt3__article">
+      <img src={img} alt="Blog Image" />
+      <div className="gpt3__article-content">
+        <span>{date}</span>
+        <h3>{title}</h3>
+        <a href={blogUrl}>Read Full Article</a>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./blog.css";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 
 const Blog = () => {
   return (
@@ -8,7 +10,36 @@ const Blog = () => {
         A lot is happening, We are blogging about it.
       </h2>
       <div className="gpt3__blog-container">
-        <Article />
+        <Article
+          img={blog01}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          blogUrl="#"
+        />
+        <Article
+          img={blog02}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          blogUrl="#"
+        />
+        <Article
+          img={blog03}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          blogUrl="#"
+        />
+        <Article
+          img={blog04}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          blogUrl="#"
+        />
+        <Article
+          img={blog05}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          blogUrl="#"
+        />
       </div>
     </div>
   );
